f93205b05619eb6f02449289f0117ef4963f40d5
Erick Carpes <erickcarpesmmc@gmail.com>
2026-05-16 12:56:22 -0300

    feat(UC7): implementar caso de uso Pagar Pedido e atualizar fakes

--- src/main/java/com/example/pizzaria/Dominio/Servicos/Fakes/EntregaServiceFake.java
+++ src/main/java/com/example/pizzaria/Dominio/Servicos/Fakes/EntregaServiceFake.java
@@ -2,14 +2,21 @@ package com.example.pizzaria.Dominio.Servicos.Fakes;
 
 import org.springframework.stereotype.Service;
 
+import com.example.pizzaria.Dominio.Dados.PedidoRepository;
 import com.example.pizzaria.Dominio.Entidades.Pedido;
 import com.example.pizzaria.Dominio.Servicos.EntregaService;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class EntregaServiceFake implements EntregaService {
+
+    private final PedidoRepository pedidoRepository;
 
     @Override
     public void agendarEntrega(Pedido pedido) {
         pedido.setStatus(Pedido.Status.TRANSPORTE);
+        pedidoRepository.atualizar(pedido);
     }
 }
